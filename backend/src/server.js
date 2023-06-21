6c76555bfd16db8a83f2d3d77bda42ed9d28ff2f
Mauro Victor <maurovictor13@gmail.com>
2023-06-20 21:49:13 -0300

    Added dotenv in project to manager environment variables.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
+import dotenv from 'dotenv';
+
 import express from "express";
 import { router } from "./Routes/routes.js";
+
+dotenv.config()
 
 const server = express();
 
 server.use(express.json());
 server.use(router);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Backend app running in port ${port}`)
